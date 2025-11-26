dba9312563d8da731221c6d5614c8d7af607147b
William Leighton Dawson <leighton@thinkst.com>
2025-11-26 19:52:34 +0200

    Add rough mocking for `RPi.GPIO`

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # hw25-astro
+
+To run outside of Raspbian you first need to mock out `RPi.GPIO`:
+```
+ln -s RPi_mock RPi
+```
---
+++ RPi_mock/GPIO.py
@@ -0,0 +1,31 @@
+import sys
+
+XDIR = 22      # Direction pin
+ZDIR = 9
+XSTEP =  27    # Step pin
+ZSTEP = 10
+
+OUT = 'OUT'
+BCM = 'BCM'
+LOW = 'LOW'
+HIGH = 'HIGH'
+
+def setup(pin, io_dir):
+    print(f'GPIO setup {pin=} {io_dir=}')
+
+def setmode(mode):
+    print(f'GPIO setmode {mode}')
+
+def output(pin, value):
+    if pin in [XSTEP, ZSTEP]:
+        sys.stdout.flush()
+        char = {HIGH: '-', LOW:'_'}[value]
+        sys.stdout.write(char)
+        sys.stdout.flush()
+    # print(f'GPIO output {pin=} {value=}')
+
+def cleanup():
+    print(f'\n\ncleanup')
+
+
+
